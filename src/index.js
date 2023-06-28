e71b3f9347bb96f97f6ac699ed35fa4d905c2f04
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-06-28 16:39:11 +0300

    Final changes made to the directory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
-// Your code here
+document.addEventListener('DOMContentLoaded', () => {
+    const characterBar = document.querySelector('#character-bar');
+    const detailedInfo = document.querySelector('#detailed-info');
+    const voteCount = document.querySelector('#vote-count');
+    const votesForm = document.querySelector('#votes-form');
+    
+    // Function to fetch characters data from the server
+    const fetchCharacters = async () => {
+      const response = await fetch('http://localhost:3000/characters');
+      const characters = await response.json();
+      return characters;
+    };
+    
+    // Function to fetch a character's details by ID
+    const fetchCharacterDetails = async (id) => {
+      const response = await fetch(`http://localhost:3000/characters/${id}`);
+      const character = await response.json();
+      return character;
+    };
+    
+    // Function to update the character's details in the detailed-info section
+    const updateCharacterDetails = (character) => {
+      const characterName = document.querySelector('#name');
+      const characterImage = document.querySelector('#image');
+    
+      characterName.textContent = character.name;
+      characterImage.src = character.image;
+      voteCount.textContent = character.votes;
+    };
+    
+    // Function to handle when a character is clicked
+    const handleCharacterClick = async (event) => {
+      if (event.target.tagName === 'SPAN') {
+        const characterId = event.target.dataset.id;
+        const character = await fetchCharacterDetails(characterId);
+        updateCharacterDetails(character);
+      }
+    };
+    
+    // Function to handle when the votes form is submitted
+    const handleVotesFormSubmit = (event) => {
+      event.preventDefault();
+    
+      const votesInput = document.querySelector('#votes');
+      const votes = parseInt(votesInput.value);
+    
+      if (!isNaN(votes) && votes >= 0) {
+        const currentVotes = parseInt(voteCount.textContent);
+        const totalVotes = currentVotes + votes;
+        voteCount.textContent = totalVotes;
+      }
+    
+      votesInput.value = '';
+    };
+    
+    // Add event listener to character bar for click events
+    characterBar.addEventListener('click', handleCharacterClick);
+    
+    // Add event listener to votes form for form submission
+    votesForm.addEventListener('submit', handleVotesFormSubmit);
+    
+    // Fetch characters and populate the character bar
+    fetchCharacters().then(characters => {
+      characters.forEach(character => {
+        const span = document.createElement('span');
+        span.textContent = character.name;
+        span.dataset.id = character.id;
+        characterBar.appendChild(span);
+      });
+    });
+  });
+  
